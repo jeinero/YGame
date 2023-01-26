ba7ca2a0c77b8ba6b62159f10a394b63b8fed410
Arthur Bertier <zups@MacBook-Air-de-Arthur.local>
2023-01-26 16:29:35 +0100

    l

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,22 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
-    public float playerScore = 0;
-    private void OnCollisionEnter(Collision collision)
+    private float level;
+    public TextMeshProUGUI showLevel;
+    // Start is called before the first frame update
+    void Start()
     {
-        if (collision.gameObject.tag == "Ennemie")
-        {
-            Destroy(collision.gameObject);
-            playerScore++;
-        }
-        else
-        {
-            
-        }
+        this.level = 0;
+        showLevel.text = $"LVL: {this.level}";
     }
-  
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        Vector3 direction = new(horizontal, vertical);
+        Move(direction);
+
+    }
+
+    private void Move(Vector3 direction)
+    {
+        transform.position += 0.25f * direction;
+
+    }
 }
